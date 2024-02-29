188e7caf9a0682fc8b9e10528d0d085828de43b2
JuanDiego500 <jdgiraldo98535@umanizales.edu.co>
2024-02-29 17:35:47 -0500

    first restaurant created

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ require('./DataBase/sync.js');
 const express = require('express');
 const connection = require('./DataBase/connection.js');
 const app = express();
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 1336;
+//routers
+const restaurantRouter = require('./Routers/restaurantRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 connection.sync({force: false})
-.then(() => { 
+  .then(() => { 
     console.log('DataBase synchronized'); 
     app.listen(port, ()=>{
         console.log("the application is running at the port: " + port);
@@ -17,4 +19,7 @@ connection.sync({force: false})
   })
   .catch((error) => {
     console.error('Mistake synchronizing the DataBase ' + error);
-  }); 
+  });
+   
+  //api
+  app.use('/api',restaurantRouter);
